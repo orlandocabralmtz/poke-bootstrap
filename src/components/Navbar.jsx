63b8ee04d899fc16c2b69b8899077bc21558a90b
Orlando Cabral <orlandocabral@live.com>
2023-05-11 09:57:28 -0500

    se crea el listado de pokemones con imagenes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+
 const Navbar = () => {
   return (
-    <h1>Navbar</h1>
+
+    <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
+      <nav className='navbar-brand' to='#'>Pokédex</nav>
+
+      <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+        <li className='nav-item px-2'>
+          <nav className='nav-link' to='/'>Home</nav>
+        </li>
+        <li className='nav-item px-2'>
+          <nav className='nav-link' to='/about'>Acerca de Pokémon</nav>
+        </li>
+      </ul>
+
+    </nav>
   )
 }
 export default Navbar
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,35 @@
+import { useState, useEffect } from 'react'
+
 const Home = () => {
+  const [pokemons, setPokemons] = useState([]) // lista de pokemon
+  const [searchTerm, setSearchTerm] = useState('') // palabra a buscar
+
+  useEffect(() => { // el useEffect se ejecuta cuando el componente se monta para hacer una petición a la API
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151') // fetch es una función de JS que hace una petición a una API y devuelve una promesa
+      .then(response => response.json()) // response.json() es una función que devuelve una promesa con los datos de la API
+      .then(data => { // data es el objeto que devuelve la promesa
+        setPokemons(data.results) // setPokemons es una función que actualiza el estado de pokemons
+      }).catch(error => console.error(error)) // catch es una función que captura el error en caso de que la promesa falle
+  }, []) // se deja el array vacío para que el useEffect se ejecute solo una vez
   return (
-    <h1>Home</h1>
+    <div className='container'>
+      <h1>Home</h1>
+      <div className='row'>
+        {
+        pokemons.map(pokemon => ( // map es una función que recorre un array y devuelve un nuevo array
+          <div className='col-sm-4 mb-4' key={pokemon.name}>
+            <div className='card'>
+              <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} alt='' />
+              <div className='card-body'>
+                <h5 className='card-title'>{pokemon.name}</h5>
+              </div>
+            </div>
+          </div>
+
+        ))
+      }
+      </div>
+    </div>
   )
 }
 export default Home
