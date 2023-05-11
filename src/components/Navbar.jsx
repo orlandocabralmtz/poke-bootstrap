3ed93e9d573dd00c66913ff1ea12fef4999b6052
Orlando Cabral <orlandocabral@live.com>
2023-05-11 12:37:54 -0500

    Header con NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
+import { NavLink } from 'react-router-dom' // NavLink es un componente de react-router-dom que se utiliza para crear links en la aplicación. reemplaza a la etiqueta <a> de HTML
 
 const Navbar = () => {
   return (
 
     <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
-      <nav className='navbar-brand' to='#'>Pokédex</nav>
+      <NavLink className='navbar-brand' to='#'>Pokédex</NavLink>  {/* to es el atributo que indica a donde va el link. Reemplaza la etiqueta href de HTML */}
 
       <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
         <li className='nav-item px-2'>
-          <nav className='nav-link' to='/'>Home</nav>
+          <NavLink className='nav-link' to='/'>Home</NavLink>
         </li>
         <li className='nav-item px-2'>
-          <nav className='nav-link' to='/about'>Acerca de Pokémon</nav>
+          <NavLink className='nav-link' to='/about'>Acerca de Pokémon</NavLink>
         </li>
       </ul>
 
